7851e987cdf29cab95de19a5f9ab4630abd918fa
swpd <swpdtz@gmail.com>
2014-08-01 11:24:45 +0000

    remove coro package and use dthread api of duda instead
    Signed-off-by: swpd <swpdtz@gmail.com>

--- main.c
+++ main.c
@@ -1,35 +1,34 @@
 #include "webservice.h"
-#include "packages/coro/coro.h"
 
-DUDA_REGISTER("Coro Examples", "coro demo");
+DUDA_REGISTER("Dthread Examples", "dthread demo");
 
 void hello(void *data)
 {
     duda_request_t *dr = data;
     response->http_status(dr, 200);
-    response->printf(dr, "hello coro");
+    response->printf(dr, "hello dthread");
     response->end(dr, NULL);
 }
 
-void cb_coro(duda_request_t *dr)
+void cb_dthread(duda_request_t *dr)
 {
-    int id = coro->create(hello, dr);
-    coro->resume(id);
+    int id = dthread->create(hello, dr);
+    dthread->resume(id);
 }
 
 struct bundle {
-    channel_t *chan;
+    duda_dthread_channel_t *chan;
     duda_request_t *dr;
 };
 
 void consumer(void *data)
 {
     struct bundle *bdl = data;
-    channel_t *chan = bdl->chan;
+    duda_dthread_channel_t *chan = bdl->chan;
     duda_request_t *dr = bdl->dr;
     response->http_status(dr, 200);
-    while (!coro->chan_done(chan)) {
-        int *n = coro->chan_recv(chan);
+    while (!dthread->chan_done(chan)) {
+        int *n = dthread->chan_recv(chan);
         response->printf(dr, "%d\n", *n);
         monkey->mem_free(n);
     }
@@ -38,36 +37,35 @@ void consumer(void *data)
 
 void productor(void *data)
 {
-    channel_t *chan = data;
+    duda_dthread_channel_t *chan = data;
     int i;
     for (i = 0; i < 100; ++i) {
         int *n = monkey->mem_alloc(sizeof(int));
         *n = i + 1;
-        coro->chan_send(chan, n);
+        dthread->chan_send(chan, n);
     }
-    coro->chan_end(chan);
+    dthread->chan_end(chan);
 }
 
 void cb_chan(duda_request_t *dr)
 {
     // unbuffered channel
-    channel_t *chan = coro->chan_create(0, monkey->mem_free);
+    duda_dthread_channel_t *chan = dthread->chan_create(0, monkey->mem_free);
     struct bundle *bdl = monkey->mem_alloc(sizeof(*bdl));
     bdl->chan = chan;
     bdl->dr = dr;
-    int cid = coro->create(consumer, bdl);
-    int pid = coro->create(productor, chan);
-    coro->chan_set_sender(chan, pid);
-    coro->chan_set_receiver(chan, cid);
-    coro->resume(cid);
-    coro->chan_free(chan);
+    int cid = dthread->create(consumer, bdl);
+    int pid = dthread->create(productor, chan);
+    dthread->chan_set_sender(chan, pid);
+    dthread->chan_set_receiver(chan, cid);
+    dthread->resume(cid);
+    dthread->chan_free(chan);
     monkey->mem_free(bdl);
 }
 
 int duda_main()
 {
-    duda_load_package(coro, "coro");
-    map->static_add("/hello/", "cb_coro");
+    map->static_add("/hello/", "cb_dthread");
     map->static_add("/chan/", "cb_chan");
     return 0;
 }
